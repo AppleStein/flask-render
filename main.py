a8dfd9df5cbc655f7335f568ac1e1e61e9bfcc6b
AppleStein <pianonono1129@gmail.com>
2022-04-23 21:53:03 +0900

    変

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-from crypt import methods
-from django.shortcuts import render
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def mainpage():
-    return render.template("mainpage.html")
+    return render_template("mainpage.html")
 
-@app.route("/", methods=["GET", "POST"])
-def namepage():
-    return render.template("name.html")
+@app.route("/<name>", methods=["GET", "POST"])
+def namepage(name):
+    return render_template("name.html")
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8888, threaded=True)
