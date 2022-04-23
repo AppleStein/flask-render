0847cb7637726c23aff1b46a5d685974be73eb94
AppleStein <pianonono1129@gmail.com>
2022-04-23 21:57:56 +0900

    変

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def mainpage():
 
 @app.route("/<name>", methods=["GET", "POST"])
 def namepage(name):
-    return render_template("name.html")
+    return render_template("name.html", name=name)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8888, threaded=True)
